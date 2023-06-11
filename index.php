be908e2da30c0f26e66280d35ee037ff7ac252a1
wimdetroyer <wimdetroyer@gmail.com>
2023-06-11 18:09:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html><head><title></title></head>
+<frameset cols='*'>
+<frame name='main' src='https://www.demusculatuur.be' scrolling='auto' noresize>
+<noframes>
+<body>
+Your browser does not support frames
+</body></noframes></frameset>
+</html>
